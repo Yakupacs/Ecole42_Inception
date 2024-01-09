5edcf08371fcc6d863032549aafcbfa9e8b22f36
yakupacs <ykpacs@gmail.com>
2024-01-10 02:25:52 +0300

    Updated.

--- inception/srcs/requirements/wordpress/tools/create_wordpress.sh
+++ inception/srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -1,22 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ -f ./wp-config.php ]
-then
-	echo "Wordpress already downloaded"
-else
+sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
+chown -R www-data:www-data /var/www/*;
+chown -R 755 /var/www/*;
+mkdir -p /run/php/;
+touch /run/php/php7.3-fpm.pid;
 
-	wget http://wordpress.org/latest.tar.gz
-	tar xfz latest.tar.gz
-	mv wordpress/* .
-	rm -rf latest.tar.gz
-	rm -rf wordpress
-
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	cp wp-config-sample.php wp-config.php
-	
+if [ ! -f /var/www/html/wp-config.php ]; then
+	echo "Wordpress: setting up..."
+	mkdir -p /var/www/html
+	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
+	chmod +x wp-cli.phar; 
+	mv wp-cli.phar /usr/local/bin/wp;
+	cd /var/www/html;
+	wp core download --allow-root;
+	mv /var/www/wp-config.php /var/www/html/
+	echo "Wordpress: creating users..."
+	wp core install --allow-root --url=${DOMAIN_NAME} --title=${WORDPRS_TTL} --admin_user=${WORDPRS_ADMIN} --admin_password=${WORDPRS_ADMIN_PASS} --admin_email=${WORDPRS_ADMIN_MAIL}
+	wp user create --allow-root ${WORDPRS_USR} ${WORDPRS_USR_MAIL} --user_pass=${WORDPRS_USR_PASS};
+	echo "Wordpress: set up!"
 fi
 
 exec "$@"
