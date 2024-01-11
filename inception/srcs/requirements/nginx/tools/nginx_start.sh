94982e9b48700d5f9bf87291d34e6ef1e56242d4
yakupacs <ykpacs@gmail.com>
2024-01-11 12:04:53 +0300

    Reloaded.

--- inception/srcs/requirements/nginx/tools/nginx_start.sh
+++ inception/srcs/requirements/nginx/tools/nginx_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ ! -f /etc/nginx/ssl/nginx.crt ]; then
+if [ ! -f /etc/ssl/certs/nginx.crt ]; then
 echo "Nginx: setting up ssl ...";
-openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/C=TR/ST=ISTANBUL/L=SARIYER/O=42Istanbul/CN=${DOMAIN_NAME}";
+openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=TR/ST=ISTANBUL/L=SARIYER/O=42Istanbul/CN=yacis.42.fr";
 echo "Nginx: ssl is set up!";
 fi
 
